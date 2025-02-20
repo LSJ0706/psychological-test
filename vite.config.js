feec73b1f427acc8b9c4aafadb59f246e385ca57
LSJ0706 <zosksldhs@gmail.com>
2025-02-20 17:52:27 +0900

    chore: project setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@commons', replacement: '/src/components/commons' },
+      { find: '@features', replacement: '/src/components/features' },
+      { find: '@layouts', replacement: '/src/components/layouts' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@hooks', replacement: '/src/libs/hooks' },
+      { find: '@api', replacement: '/src/libs/api' },
+      { find: '@utils', replacement: '/src/libs/utils' }
+    ]
+  }
+});
