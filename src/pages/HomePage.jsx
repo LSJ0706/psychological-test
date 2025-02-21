0719a3d36ede7cd353c491188f3c6719e4b225e1
LSJ0706 <zosksldhs@gmail.com>
2025-02-21 20:44:18 +0900

    feat: myPage,SignInPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
-import { getTestResults, createTestResult, deleteTestResult, updateTestResultVisibility } from '@apis/testResults';
+import { useAuthStore } from '../store/store';
+
 const home = () => {
-  const getHandler = async () => {
-    const data = await getTestResults();
-    console.log(data);
-  };
-  const postHandler = async () => {
-    const resultData = {
-      nickname: '닉네임',
-      result: 'MBTI 유형',
-      visibility: true
-    };
-    console.log(await createTestResult(resultData));
-  };
-  const deleteHandler = async () => {
-    console.log(await deleteTestResult('유저 ID'));
-  };
-  const patchHandler = async () => {
-    console.log(await updateTestResultVisibility(6086, { visibility: false }));
-  };
+  const { userInfo, userSignOut } = useAuthStore((state) => state); // token 값 직접 가져오기
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-blue-200 ">
-      <h2>무료 성격 테스트</h2>
-      <p>자신의 성격 유형을 확인할 수 있도록 솔직하게 답변해주세요.</p>
-      <br />
-      <button onClick={getHandler} className="bg-blue-500 hover:bg-fuchsia-500">
-        get 해보기
-      </button>
-      <br />
-      <button onClick={postHandler}>post 해보기</button>
-      <br />
-      <button onClick={deleteHandler}>delete 해보기</button>
-      <br />
-      <button onClick={patchHandler}>patch 해보기</button>
+    <div>
+      {userInfo.token ? (
+        <h1>
+          로그인 성공! <button onClick={() => userSignOut()}>로그아웃</button>
+        </h1>
+      ) : (
+        <h1>로그인 상태 아님</h1>
+      )}
     </div>
   );
 };
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { signIn } from '@apis/auth';
+import Form from '@commons/Form';
+import { useMutation } from '@tanstack/react-query';
+import { useAuthStore } from '../store/store';
+import { useNavigate } from 'react-router-dom';
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const { userSignIn } = useAuthStore((state) => state);
+  const navigate = useNavigate();
+
+  const signInMutation = useMutation({
+    mutationFn: signIn,
+    onSuccess: (data) => {
+      alert('로그인 성공');
+      userSignIn({ token: data.accessToken, userId: data.userId, nickname: data.nickname });
+      navigate('/');
+    },
+    onError: (error) => alert(error.response.data.message)
+  });
+
+  const inputs = [
+    { name: 'id', type: 'email', placeholder: '이메일을 입력해주세요' },
+    { name: 'password', type: 'password', placeholder: '비밀번호를 입력해주세요' }
+  ];
+
+  return (
+    <div>
+      <Form title="로그인" inputs={inputs} buttonText="로그인" onSubmit={signInMutation.mutate} />
+    </div>
+  );
 };
 
 export default SignInPage;
